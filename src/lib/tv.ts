a1ae6a47f56822401f974729fb676336d490127c
kazaminn <90266173+kazaminn@users.noreply.github.com>
2026-02-25 11:09:55 +0900

    ✨ feat(lib): add composeProps function
    - merge with React-Aria's render props
      (props = className + function)

--- src/lib/tv.ts
+++ src/lib/tv.ts
@@ -1,7 +1,23 @@
+import { twMerge } from 'tailwind-merge';
 import { type VariantProps, createTV } from 'tailwind-variants';
 
 export const tv = createTV({
   twMerge: true,
 });
+
+export function composeProps<T>(
+  className: string | ((v: T) => string) | undefined,
+  tw: string | ((v: T) => string)
+): (v: T) => string {
+  return (renderProps) => {
+    const twClass = typeof tw === 'function' ? tw(renderProps) : tw;
+    const cls =
+      typeof className === 'function' ? className(renderProps) : className;
+    return twMerge(twClass, cls);
+  };
+}
+
+// sampleコピペをそのまま使えるようにaliasを用意
+export const composeTailwindRenderProps = composeProps;
 
 export type { VariantProps };
